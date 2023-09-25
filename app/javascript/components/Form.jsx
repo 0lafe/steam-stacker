5c6d7fb5a6b22db08a0479eaefc34b3a5f4e8851
lafe <0lafethehorsescrub@gmail.com>
2023-09-25 17:14:26 -0500

    added filters

--- app/javascript/components/Form.jsx
+++ app/javascript/components/Form.jsx
@@ -6,13 +6,12 @@ const Form = () => {
   const [userID, setUserID] = useState('')
   const [appID, setAppID] = useState('')
   const [apiKey, setKey] = useState('')
+  const [filter, setFilter] = useState('')
+  const [filterQuantity, setFilterQuantity] = useState('')
 
   return (
     <>
-      <p>Please Fill Out All Fields</p>
-      <p>You can find your steam ID <strong><a href='https://steamid.xyz/'>here</a></strong></p>
-      <p>You can find steam game IDs <strong><a href='https://steamdb.info/'>here</a></strong></p>
-      <p>You can get a steam API key <strong><a href='https://steamcommunity.com/dev/apikey'>here</a></strong></p>
+      <h1>Please Fill Out All Fields</h1>
       <br/>
       <br/>
       <TextField
@@ -22,6 +21,7 @@ const Form = () => {
         onChange={(e) => {setUserID(e.target.value)}}
         />
       <br/>
+      <p>You can find your steam ID <strong><a href='https://steamid.xyz/'>here</a></strong></p>
       <br/>
       <TextField
         required
@@ -30,6 +30,7 @@ const Form = () => {
         onChange={(e) => {setAppID(e.target.value)}}
         />
       <br/>
+      <p>You can find steam game IDs <strong><a href='https://steamdb.info/'>here</a></strong></p>
       <br/>
       <TextField
         required
@@ -37,9 +38,30 @@ const Form = () => {
         label="API Key (opbtained from steam)"
         onChange={(e) => {setKey(e.target.value)}}
         />
+      <p>You can get a steam API key <strong><a href='https://steamcommunity.com/dev/apikey'>here</a></strong></p>
+      <br/>
+      <h3>~ Optional ~</h3>
+      <div>
+      <TextField
+        required
+        id="filter"
+        label="Filter"
+        onChange={(e) => {setFilter(e.target.value)}}
+        style={{margin: '0 2rem'}}
+        />
+      <TextField
+        required
+        id="filter-quantity"
+        label="Quantity"
+        onChange={(e) => {setFilterQuantity(e.target.value)}}
+        style={{margin: '0 2rem'}}
+        />
+      </div>
+      <p>Optional filter for specifying items to stack/unstack</p>
+      <p>Filter will work on partial matches</p>
       <br/>
       <br/>
-      <Consolidator userID={userID} appID={appID} apiKey={apiKey}/>
+      <Consolidator userID={userID} appID={appID} apiKey={apiKey} filter={filter} filterQuantity={filterQuantity}/>
     </>
   )
 }
